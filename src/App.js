22ea1d8efd06be543a7071f2235027dae8a764bc
Divyashree Y <136325917+Divyashreey@users.noreply.github.com>
2024-06-23 16:45:54 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/home/Home";
 import SinglePost from "./components/singlePost/SinglePost";
 import Write from "./pages/write/Write";
 import Register from "./pages/register/Register";
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import {BrowserRouter,Switch,Route} from "react-router-dom";
 import SinglePost1 from "./components/singlePost/SinglePost1";
 import SinglePost2 from "./components/singlePost/SinglePost2";
 import SinglePost3 from "./components/singlePost/SinglePost3";
@@ -29,7 +29,7 @@ function App() {
     <BrowserRouter>
  <Topbar/>
   
-  <Routes>
+  <Switch>
   <Route  exact path="/"> <Home/></Route>
   <Route path="/register"> {user?<Home/>:<Register/>}</Route>
   <Route path="/login">{user?<Home/>:<Login/>}</Route>
@@ -52,7 +52,7 @@ function App() {
         <Route path="/admin" component={Admin} />
      
 
-  </Routes>
+  </Sw>
   
   </BrowserRouter>
   );
